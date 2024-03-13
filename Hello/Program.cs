29e7352ade03990dbe7fe4982f475c61ea998e1e
NovelBiswas <novelbiswas@gmail.com>
2024-03-14 01:37:25 +0530

    Add project files.

---
+++ Hello/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I am Novel BIswas.");
+            Console.Write("I am Learning C#.");
+            Console.ReadLine();
+        }
+    }
+}
